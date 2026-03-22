e4d199fc7e055c585850d46a7dd12a4d5bb3d0fb
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-03-22 13:59:33 -0300

    master: feat (00_hello_world) - Added Hello World project (basic and advanced versions)
    - Introduced a Hello World module with both a simple and a styled interactive version.
    - Also updated the Christmas Gift project to improve navigation flow between modules.

---
+++ projects/00_hello_world/v1/scripts/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const title = document.getElementById("title");
+
+button.addEventListener("click", () => {
+  title.textContent = "Hello World 👋";
+});
